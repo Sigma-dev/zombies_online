107ff48aca3defd39ee8ff5a44ef168489577869
Sigma-dev <antonin.duthil@epitech.eu>
2025-04-07 20:10:18 +0200

    (fix): added transform interpolation

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ mod zo;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins((PhysicsPlugins::default(), PhysicsDebugPlugin::default()))
+        .add_plugins((
+            PhysicsPlugins::default().set(PhysicsInterpolationPlugin::interpolate_all()),
+            PhysicsDebugPlugin::default(),
+        ))
         .insert_resource(Gravity::ZERO)
         .add_plugins(ZOPlugin)
         .run();
